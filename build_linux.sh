28b85719b37758da7d53be4788d9f56b5e9660ce
Jakub Wasylków <kuba_160@protonmail.com>
2021-04-22 16:57:10 +0200

    8

--- build_linux.sh
+++ build_linux.sh
@@ -2,25 +2,23 @@
 set -x
 export VERSION=5.15.2
 export QT_GIT=git://code.qt.io/qt/qt5.git
-export BUILD_DIR="qt5-build"
-export OUTPUT_DIR="output"
+export OUTPUT_DIR="qt5_static"
 
-git clone -b $VERSION --single-branch $QT_GIT 	;true
+git clone -b $VERSION --single-branch $QT_GIT   ;true
+perl qt5/init-repository --module-subset=default,-qtwebengine
 cd qt5; git submodule update --init --recursive ;true
 
-mkdir -p "$BUILD_DIR"
-
-cd "$BUILD_DIR"; ../qt5/configure -confirm-license \
-	-opensource -nomake examples -nomake tests -static \
-	-prefix         $BUILD_DIR \
-    -sysconfdir     /etc/xdg   \
-    -dbus-linked               \
-    -openssl-linked            \
-    -system-harfbuzz           \
-    -system-sqlite             \
-    -no-rpath                  \
+qt5/configure -confirm-license \
+    -opensource -nomake examples -nomake tests -static \
+    -prefix $OUTPUT_DIR     \
+    -sysconfdir /etc/xdg    \
+    -dbus-linked            \
+    -openssl-linked         \
+    -system-harfbuzz        \
+    -system-sqlite          \
+    -no-rpath               \
     -skip qtwebengine
 
-make -C "$BUILD_DIR"
+make
 make install
-zip -r qt_$VERSION_static_linux.zip
+zip -r qt_$VERSION_static_linux.zip qt5_qtatic
