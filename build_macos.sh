6d25a26b149e606027e68dc43d1837066138c766
Jakub Wasylków <kuba_160@protonmail.com>
2021-05-03 17:22:26 +0200

    Add macOS build

---
+++ build_macos.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -x
+export VERSION=5.12.10
+export QT_GIT=git://code.qt.io/qt/qt5.git
+export OUTPUT_DIR="`pwd`/qt5/build/qt5_static"
+
+# python fix
+ln -s /usr/bin/python3 /usr/bin/python; true
+
+git clone -b $VERSION --single-branch $QT_GIT; true
+cd qt5;
+
+perl init-repository --module-subset=default,-qtwebengine
+
+mkdir build
+cd build
+
+../configure -confirm-license \
+    -opensource -nomake examples -nomake tests -static \
+    -release                \
+    -prefix $OUTPUT_DIR     \
+    -sysconfdir /etc/xdg    \
+    -system-zlib            \
+    -qt-libpng              \
+    -qt-libjpeg             \
+    -qt-freetype            \
+    -qt-pcre                \
+    -no-rpath               \
+    -no-icu                 \
+    -dbus-runtime           \
+    -skip qtwebengine
+    # missing iconv
+
+make
+make install
+
+cd $OUTPUT_DIR/..
+zip -r "qt_$VERSION"_static_macos.zip `basename $OUTPUT_DIR`
